73eb9dd369e5117915478f2f4a93f01a09e384b8
CameronL2 <127059233+Yomi8@users.noreply.github.com>
2023-03-07 02:10:00 +0000

    Added user selection

--- main.py
+++ main.py
@@ -6,3 +6,14 @@
   
   TIP: Use assessment guide to help guide you through this Internal
 '''
+# packages
+
+# functions
+
+# main routine
+print("#*#*#*# Quiz Creator #*#*#*#")
+print("OPTIONS")
+print("A - Play the default quiz")
+print("B - Create a new quiz")
+print("C - Import an existing quiz")
+user_selection = input("Your selection (A/B/C): ")
