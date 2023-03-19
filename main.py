3a976a1aac57fbd8e70652c729269a62b32333f8
CameronL2 <127059233+Yomi8@users.noreply.github.com>
2023-03-19 22:53:08 +0000

    Fixed capitalization in quizzes, including the default one

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def export_quiz(quiz):
   file.write(jsonstr)
   file.close()
   print("Export Successful!")
+  print("File saved as quiz.json")
 
 def create_quiz():
   # Clear the console
@@ -47,7 +48,7 @@ def create_quiz():
     # Asks user for question
     question = input(f"Enter question {i+1}: ")
     # Asks user for choices for question
-    choices = input(f"Enter the answer choices for question {i+1}, separated by commas: ").upper().split(",")
+    choices = input(f"Enter the answer choices for question {i+1}, separated by commas: ").split(",")
     lettered_choices = {}
     # Gives an index letter (A - Z) for each choice
     for index, choice in enumerate(choices):
@@ -96,13 +97,15 @@ def run_quiz(quiz):
     question_num = question_data_list[question_list.index(question)]['IndexNum']
     # Seperates choices dictionary into a variable
     choices = question_data_list[question_list.index(question)]['choices']
+    # Copies the choices dictionary for use in checking answers in all caps
+    upper_choices = {key: value.upper() if isinstance(value, str) else value for key, value in choices.items()}
     # Seperates keys of the choices dictionary into a list
     letter_indexs_list = [key for key in choices]
     # Creates a usable output for the choices keys
     letter_index_output = '/'.join(letter_indexs_list)
     # Seperates answer into it's own dictionary
     long_answer = question_data_list[question_list.index(question)]['answer']
-    for index, value in choices.items():
+    for index, value in upper_choices.items():
       if value == long_answer:
           matching_index = index
           break
@@ -112,7 +115,7 @@ def run_quiz(quiz):
     print(question)
     # Prints all avaliable options and their index letters
     for letter_index, full_answer in choices.items():
-      print(letter_index, full_answer.capitalize())
+      print(letter_index, full_answer)
     # user input
     user_answer = input(f"Select your answer ({letter_index_output}): ").upper()
     # Checks user input against answer dictionary
@@ -137,15 +140,9 @@ def run_quiz(quiz):
   print(f"You scored {score} out of {len(quiz)} questions!")
   time.sleep(5)
   restart()
-  
-
-
-
-
 
 # main routine
 # Print options
-
 
 print("#*#*#*# Quiz Creator #*#*#*#")
 print("OPTIONS")
@@ -157,7 +154,208 @@ print("3 - Import and play an existing quiz")
 user_selection = input("Your selection (1/2/3): ").lower()
 # Selection
 if user_selection == "1":
-  quiz = {'What is the capital of Japan?': {'IndexNum': 1, 'choices': {'A': 'BEIJING', 'B': 'SEOUL', 'C': 'TOKYO', 'D': 'HANOI'}, 'answer': 'TOKYO'}, 'Which of the following is the smallest planet in our solar system?': {'IndexNum': 2, 'choices': {'A': 'EATH', 'B': 'MARS', 'C': 'MERCURY', 'D': 'VENUS'}, 'answer': 'MERCURY'}, 'Which of the following is not a type of rock?': {'IndexNum': 3, 'choices': {'A': 'IGNEOUS', 'B': 'METAMORPHIC', 'C': 'SEDIMENTARY', 'D': 'ORGANIC'}, 'answer': 'ORGANIC'}, 'Who is the author of the Harry Potter series?': {'IndexNum': 4, 'choices': {'A': 'STEPHEN KING', 'B': 'JK ROWLING', 'C': 'SUZANNE COLLINS', 'D': 'GEORGE RR MARTIN'}, 'answer': 'JK ROWLING'}, 'What is the tallest mountain in the world?': {'IndexNum': 5, 'choices': {'A': 'MOUNT EVEREST', 'B': 'K2', 'C': 'MOUNT KILLIMANJARO', 'D': 'MOUNT DENALI'}, 'answer': 'MOUNT EVEREST'}, 'Which of the following is a programming language?': {'IndexNum': 6, 'choices': {'A': 'HTML', 'B': 'XML', 'C': 'JAVA', 'D': 'SQL'}, 'answer': 'JAVA'}, 'Who invented the telephone?': {'IndexNum': 7, 'choices': {'A': 'ALEXANDER GRAHAM BELL', 'B': 'THOMAS EDISON', 'C': 'ALBERT EINSTEIN', 'D': 'ISAAC NEWTON'}, 'answer': ''}, 'What is the largest country by land area?': {'IndexNum': 8, 'choices': {'A': 'UNITED STATES', 'B': 'RUSSIA', 'C': 'CHINA', 'D': 'CANADA'}, 'answer': 'RUSSIA'}, 'Which of the following is not a type of cloud?': {'IndexNum': 9, 'choices': {'A': 'STRATUS', 'B': 'CIRRUS', 'C': 'NIMBUS', 'D': 'HAZE'}, 'answer': 'HAZE'}, 'Who was the first man to walk on the moon?': {'IndexNum': 10, 'choices': {'A': 'NEIL ARMSTRONG', 'B': 'BUZZ ALDRIN', 'C': 'YURI GAGARIN', 'D': 'JOHN GLENN'}, 'answer': 'NEIL ARMSTRONG'}, 'What is the chemical symbol for gold?': {'IndexNum': 11, 'choices': {'A': 'AU', 'B': 'AG', 'C': 'CU', 'D': 'PT'}, 'answer': 'AU'}, 'Which of the following is not a type of dance?': {'IndexNum': 12, 'choices': {'A': 'SALSA', 'B': 'WALTZ', 'C': 'TANGO', 'D': 'RUMBA'}, 'answer': 'RUMBA'}, 'Which of the following is a type of pasta?': {'IndexNum': 13, 'choices': {'A': 'LASAGNA', 'B': 'BURRITO', 'C': 'SUSHI', 'D': 'FALAFEL'}, 'answer': 'LASAGNA'}, 'What is the currency of India?': {'IndexNum': 14, 'choices': {'A': 'RUPEE', 'B': 'YEN', 'C': 'EURO', 'D': 'POUND'}, 'answer': 'RUPEE'}, 'Which of the following is a type of whale?': {'IndexNum': 15, 'choices': {'A': 'DOLPHIN', 'B': 'NARWHAL', 'C': 'ORCA', 'D': 'WALRUS'}, 'answer': 'NARWHAL'}, 'Which of the following is not a type of fish?': {'IndexNum': 16, 'choices': {'A': 'TUNA', 'B': 'SALMON', 'C': 'DOLPHIN', 'D': 'COD'}, 'answer': 'DOLPHIN'}, 'Which of the following is not a type of dinosaur?': {'IndexNum': 17, 'choices': {'A': 'T-REX', 'B': 'STEGOSAURUS', 'C': 'PTERODACTYL', 'D': 'SABERTOOTH'}, 'answer': 'SABERTOOTH'}, 'Which of the following is a type of fruit?': {'IndexNum': 18, 'choices': {'A': 'CUCUMBER', 'B': 'EGGPLANT', 'C': 'TOMATO', 'D': 'STRAWBERRY'}, 'answer': 'STRAWBERRY'}, 'Which of the following is not a type of tree?': {'IndexNum': 19, 'choices': {'A': 'OAK', 'B': 'PINE', 'C': 'MAPLE', 'D': 'CORAL'}, 'answer': 'CORAL'}, "Who directed the movie 'Jaws'?": {'IndexNum': 20, 'choices': {'A': 'STEVEN SPIELBURG', 'B': 'MARTIN SCORSESE', 'C': 'QUENTIN TARANTINO', 'D': 'FRANCIS FORD COPPOLA'}, 'answer': 'STEVEN SPIELBURG'}}
+  quiz = {
+    "What is the capital of Japan?": {
+        "IndexNum": 1,
+        "choices": {
+            "A": "Beijing",
+            "B": "Seoul",
+            "C": "Tokyo",
+            "D": "Hanoi"
+        },
+        "answer": "TOKYO"
+    },
+    "Which of the following is the smallest planet in our solar system?": {
+        "IndexNum": 2,
+        "choices": {
+            "A": "Earth",
+            "B": "Mars",
+            "C": "Mercury",
+            "D": "Venus"
+        },
+        "answer": "MERCURY"
+    },
+    "Which of the following is not a type of rock?": {
+        "IndexNum": 3,
+        "choices": {
+            "A": "Igneous",
+            "B": "Metamorphic",
+            "C": "Sedimentary",
+            "D": "Organic"
+        },
+        "answer": "ORGANIC"
+    },
+    "Who is the author of the Harry Potter series?": {
+        "IndexNum": 4,
+        "choices": {
+            "A": "Stephen King",
+            "B": "JK Rowling",
+            "C": "Suzanne Collins",
+            "D": "George RR Martin"
+        },
+        "answer": "JK ROWLING"
+    },
+    "What is the tallest mountain in the world?": {
+        "IndexNum": 5,
+        "choices": {
+            "A": "Mount Everest",
+            "B": "K2",
+            "C": "Mount Kilimanjaro",
+            "D": "Mount Denali"
+        },
+        "answer": "MOUNT EVEREST"
+    },
+    "Which of the following is a programming language?": {
+        "IndexNum": 6,
+        "choices": {
+            "A": "HTML",
+            "B": "XML",
+            "C": "Java",
+            "D": "SQL"
+        },
+        "answer": "JAVA"
+    },
+    "Who invented the telephone?": {
+        "IndexNum": 7,
+        "choices": {
+            "A": "Alexander Graham Bell",
+            "B": "Thomas Edison",
+            "C": "Albert Einstein",
+            "D": "Isaac Newton"
+        },
+        "answer": "ALEXANDER GRAHAM BELL"
+    },
+    "What is the largest country by land area?": {
+        "IndexNum": 8,
+        "choices": {
+            "A": "United States",
+            "B": "Russia",
+            "C": "China",
+            "D": "Canada"
+        },
+        "answer": "RUSSIA"
+    },
+    "Which of the following is not a type of cloud?": {
+        "IndexNum": 9,
+        "choices": {
+            "A": "Stratus",
+            "B": "Cirrus",
+            "C": "Nimbus",
+            "D": "Haze"
+        },
+        "answer": "HAZE"
+    },
+    "Who was the first man to walk on the moon?": {
+        "IndexNum": 10,
+        "choices": {
+            "A": "Neil Armstrong",
+            "B": "Buzz Aldrin",
+            "C": "Yuri Gagarin",
+            "D": "John Glenn"
+        },
+        "answer": "NEIL ARMSTRONG"
+    },
+    "What is the chemical symbol for gold?": {
+        "IndexNum": 11,
+        "choices": {
+            "A": "Au",
+            "B": "Ag",
+            "C": "Cu",
+            "D": "Pt"
+        },
+        "answer": "AU"
+    },
+    "Which of the following is not a type of dance?": {
+        "IndexNum": 12,
+        "choices": {
+            "A": "Salsa",
+            "B": "Waltz",
+            "C": "Tango",
+            "D": "Rumba"
+        },
+        "answer": "RUMBA"
+    },
+    "Which of the following is a type of pasta?": {
+        "IndexNum": 13,
+        "choices": {
+            "A": "Lasagna",
+            "B": "Burrito",
+            "C": "Sushi",
+            "D": "Falafel"
+        },
+        "answer": "LASAGNA"
+    },
+    "What is the currency of India?": {
+        "IndexNum": 14,
+        "choices": {
+            "A": "Rupee",
+            "B": "Yen",
+            "C": "Euro",
+            "D": "Pound"
+        },
+        "answer": "RUPEE"
+    },
+    "Which of the following is a type of whale?": {
+        "IndexNum": 15,
+        "choices": {
+            "A": "Dolphin",
+            "B": "Narwhal",
+            "C": "Orca",
+            "D": "Walrus"
+        },
+        "answer": "NARWHAL"
+    },
+    "Which of the following is not a type of fish?": {
+        "IndexNum": 16,
+        "choices": {
+            "A": "Tuna",
+            "B": "Salmon",
+            "C": "Dolphin",
+            "D": "Cod"
+        },
+        "answer": "DOLPHIN"
+    },
+    "Which of the following is not a type of dinosaur?": {
+        "IndexNum": 17,
+        "choices": {
+            "A": "T-Rex",
+            "B": "Stegosaurus",
+            "C": "Pterodactyl",
+            "D": "Sabertooth"
+        },
+        "answer": "SABERTOOTH"
+    },
+    "Which of the following is a type of fruit?": {
+        "IndexNum": 18,
+        "choices": {
+            "A": "Cucumber",
+            "B": "Eggplant",
+            "C": "Tomato",
+            "D": "Strawberry"
+        },
+        "answer": "STRAWBERRY"
+    },
+    "Which of the following is not a type of tree?": {
+        "IndexNum": 19,
+        "choices": {
+            "A": "Oak",
+            "B": "Pine",
+            "C": "Maple",
+            "D": "Coral"
+        },
+        "answer": "CORAL"
+    },
+    "Who directed the movie 'Jaws'?": {
+        "IndexNum": 20,
+        "choices": {
+            "A": "Steven Spielberg",
+            "B": "Martin Scorsese",
+            "C": "Quentin Tarantino",
+            "D": "Francis Ford Coppola"
+        },
+        "answer": "STEVEN SPIELBURG"
+    }
+}
   run_quiz(quiz)
 elif user_selection == "2":
   create_quiz()
